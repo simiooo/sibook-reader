681603c1487f954e2e21949ef7cf107ba8e91a92
simooowann <simooowann@gmail.com>
2023-11-22 20:27:47 +0800

    修改pdf阅读器

--- src/pages/PdfReader/index.tsx
+++ src/pages/PdfReader/index.tsx
@@ -1,6 +1,6 @@
-import { Col, Menu, Row, Breadcrumb, Spin, Result } from 'antd'
+import { Col, Menu, Row, Breadcrumb, Spin, Result, Slider } from 'antd'
 import { Document, Page } from 'react-pdf';
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 import { useBookState } from '../../store';
@@ -11,6 +11,7 @@ import style from './index.module.css'
 import { pdfjs } from 'react-pdf';
 import worker from 'react-pdf/'
 import { useKeyPress } from 'ahooks';
+import { createPortal } from 'react-dom';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.js',
@@ -24,37 +25,42 @@ export default function PdfReader() {
   const { book_id } = useParams()
   const [pdf, setPdf] = useState()
 
-  const [blob, setBlob] = useState<{data: Uint8Array}>()
+  const [blob, setBlob] = useState<{ data: Uint8Array }>()
   const [numPages, setNumPages] = useState<number>();
   const [pageNumber, setPageNumber] = useState<number>(1);
   function onDocumentLoadSuccess({ numPages, ...others }: { numPages: number }): void {
-    console.log(others);
-    
     setNumPages(numPages);
   }
+
+  const renderProgress = useMemo(() => {
+    return (pageNumber / numPages) * 100
+  }, [
+    pageNumber,
+    numPages
+  ])
 
   const init = useCallback(async () => {
     return await db_instance?.transaction('rw', 'book_items', 'book_blob', async () => {
       const book_info = await db_instance.book_items.where('hash').equals(book_id).first()
       const book_blob = await db_instance.book_blob.where('id').equals(book_id).first()
       console.log(book_blob)
-      setBlob({data: book_blob.blob})
+      setBlob({ data: book_blob.blob })
       setBookInfo(book_info)
     })
   }, [])
 
   useKeyPress('uparrow', () => {
-    if(pageNumber === 0) {
+    if (pageNumber === 0) {
       return
     }
-    setPageNumber(pageNumber -1)
+    setPageNumber(pageNumber - 1)
   })
   useKeyPress(40, () => {
-    if(pageNumber === numPages) {
+    if (pageNumber === numPages) {
       return
     }
-    setPageNumber(pageNumber +1)
-    
+    setPageNumber(pageNumber + 1)
+
   })
 
   useEffect(() => {
@@ -82,7 +88,6 @@ export default function PdfReader() {
                 },
                 {
                   title: '该书籍',
-                  // onClick: () => reload()
                 }
               ]}
             ></Breadcrumb></p>
@@ -94,28 +99,69 @@ export default function PdfReader() {
         </Row>
       </Col>
       <Col span={24}>
-        <div>
-          <Row>
-            <Col>
+          <Row
+          style={{
+            width: '100%'
+          }}
+          >
+            <Col
+            xxl={4}
+            xl={4}
+            lg={6}
+            md={8}
+            span={4}
+            sm={8}
+            xs={8}
+            >
               <Menu></Menu>
             </Col>
-            <Col>
+            <Col
+            sm={16}
+            xs={16}
+            xl={20}
+            md={16}
+            lg={18}
+            span={20}
+            >
               <div className={style.pdf_container} id="pdf_container">
-                <Document 
-                loading={<Spin spinning={true}></Spin>}
-                error={<Result
-                status="error"
-                title="书籍加载失败"
-                ></Result>}
-                file={blob} onLoadSuccess={onDocumentLoadSuccess}>
-                  <Page pageNumber={pageNumber} />
+                <Document
+                
+                  className={style.pdf_document}
+                  loading={<Spin spinning={true}></Spin>}
+                  error={<Result
+                    status="error"
+                    title="书籍加载失败"
+                  ></Result>}
+                  file={blob} onLoadSuccess={onDocumentLoadSuccess}>
+                  <Page 
+                  width={1020}
+                  pageNumber={pageNumber} />
                 </Document>
               </div>
             </Col>
           </Row>
-        </div>
       </Col>
-      <Col span={24}></Col>
+      <Col span={24}>
+
+      </Col>
+      {createPortal(
+        <div className={style.reader_progress}><Row>
+          <Col
+            span={24}
+          >
+            <Slider
+              tooltip={{
+                formatter: (v) => `${v}%`
+              }}
+              onChange={(v) => {
+                setPageNumber(Math.round(v / 100 * numPages))
+              }}
+              value={renderProgress} />
+
+          </Col>
+        </Row></div>
+        , document.body
+      )}
     </Row>
   )
 }
