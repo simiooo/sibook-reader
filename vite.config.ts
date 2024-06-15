5c9f3da7cd92ecc429b0d59bae9375cf3ed0e06e
simooowann <simooowann@gmail.com>
2024-06-15 18:51:57 +0800

    修改环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://squirrelsi.top:8080/api",
+        target: `http://127.0.0.1:8080`,
         // target: "http://localhost:8080",
         // rewrite: (path) => path.replace(/^\/api/, ""),
       },
